938b3caee30758bb0d3caeae4bafc65c931f8aaf
Tealimes <alexpeacock56ten@gmail.com>
2024-12-21 20:31:29 -0500

    initialized files

---
+++ FIFO_RAM.srcs/sources_1/new/fifo_ram.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/21/2024 08:31:13 PM
+// Design Name: 
+// Module Name: fifo_ram
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fifo_ram(
+
+    );
+endmodule
